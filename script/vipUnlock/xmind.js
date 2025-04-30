63aa39123522ea200b8b5879f2c2995c2fdd3ef2
fhqievg <30724814+fhqievg@users.noreply.github.com>
2025-04-30 14:06:42 +0800

    更新 xmind.js

--- script/vipUnlock/xmind.js
+++ script/vipUnlock/xmind.js
@@ -1,4 +1,4 @@
 let obj = JSON.parse($response.body);
 obj.license.status = 'sub';
-obj.license.expireTime = 0x1b8d90e4000;
+obj.license.expireTime = 0x201a6388000;
 $done({ body: JSON.stringify(obj) });
