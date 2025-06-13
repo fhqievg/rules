bc9c648bec2c0c47c32116bbc3432f31319aa122
fhqievg <30724814+fhqievg@users.noreply.github.com>
2025-06-13 17:18:24 +0800

    更新 xiaoXiongYouHao.js

--- script/vipUnlock/xiaoXiongYouHao.js
+++ script/vipUnlock/xiaoXiongYouHao.js
@@ -6,5 +6,9 @@ if (url.includes("api/vip/index")) {
     obj.vip_valid_till_date = "2099\u5e7412\u670831\u65e5";
 } else if ( url.includes("api/account_sync")) {
     obj.accountInfo.accountSetting.phone_number = "13012345678";
+    
+    if (obj.accountInfo?.mainFloatingEntrance?.hasOwnProperty("icon_url")) {
+        obj.accountInfo.mainFloatingEntrance.icon_url = "";
+    }
 }
 $done({ body:JSON.stringify(obj) });
