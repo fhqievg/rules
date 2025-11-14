f55985f8fb76e946d1bb5349a30706ad27736363
fhqievg <30724814+fhqievg@users.noreply.github.com>
2025-11-14 13:55:05 +0800

    更新 amap.js

--- script/amap.js
+++ script/amap.js
@@ -201,10 +201,6 @@ if (url.includes("/shield/scene/recommend")) {
             "PopularActivitiesCard" //互动专区
         ];
         obj.data.cardList = obj.data.cardList.filter((i) => {
-            if (delArr.includes(i?.dataKey)) {
-                return false;
-            }
-            
             switch (i.dataKey) {
                 case "MineArrowActionCard":
                     //去除2025入口
@@ -245,6 +241,10 @@ if (url.includes("/shield/scene/recommend")) {
                     break;
                 default:
                     break;
+            }
+            
+            if (delArr.includes(i?.dataKey)) {
+                return false;
             }
             return true;
         });
