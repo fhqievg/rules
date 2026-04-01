6a434a9c709c794e6461712c649736079c7fa4d2
fhqievg <30724814+fhqievg@users.noreply.github.com>
2026-04-01 14:08:00 +0800

    更新 caiYun.js

--- script/caiYun.js
+++ script/caiYun.js
@@ -1,41 +1,40 @@
 const url = $request.url;
 let obj = JSON.parse($response.body);
+const apiUrl = "/api.caiyunapp.com";
+const wrapperUrl = "/wrapper.cyapi.cn";
 
 if (url.includes("/v3/config")) {
     //搜索建议
     if(obj?.search_suggestions) {
         obj.search_suggestions = [];
     }
-} else if (url.includes("/api.caiyunapp.com/v1/activity")) {
+} else if (url.includes("/v1/activity")) {
     if (url.includes("&type_id=A03&")) {
       // 底栏控制项目 主页图标 天气助手 彩云ai
       if (obj?.interval) {
         obj.interval = 2592000; // 30天===2592000秒
       }
       if (obj?.activities?.length > 0) {
-        for (let item of obj.activities) {
-          if (item?.name && item?.type && item?.feature) {
-            item.feature = false;
-          }
+        if (url.includes(apiUrl)) {
+            for (let item of obj.activities) {
+              if (item?.name && item?.type && item?.feature) {
+                item.feature = false;
+              }
+            }
+        } else if (url.includes(wrapperUrl)) {
+            obj.activities = [];
         }
       }
     } else {
-      // 其他请求
-      obj = { status: "ok", activities: [{ items: [] }] };
-    }
-} else if (url.includes("/wrapper.cyapi.cn/v1/activity")) {
-    // 彩云推广
-    if (["&type_id=A03&"]?.includes(url)) {
-      // 天气助手 彩云ai
-      if (obj?.interval) {
-        obj.interval = 2592000; // 30天===2592000秒
-      }
-      if (obj?.activities?.length > 0) {
-        obj.activities = [];
-      }
-    } else {
-      // 其他请求
-      obj = { status: "ok", activities: [{ items: [] }] };
+        // 其他请求
+        if (url.includes(apiUrl)) {
+            obj = { status: "ok", activities: [{ items: [] }] }
+        } else if (url.includes(wrapperUrl)) {
+            if (!url.includes("&type_id=P04&") {
+                //不处理节假日
+                obj = { status: "ok", activities: [] };
+            }
+        }
     }
 }
 
