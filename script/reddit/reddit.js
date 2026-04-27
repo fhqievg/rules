fe84edb6c1a7cc6ad1c3c825ff0eb31e0c1270a6
fhqievg <30724814+fhqievg@users.noreply.github.com>
2026-04-28 01:01:41 +0800

    Add files via upload

---
+++ script/reddit/reddit.js
@@ -0,0 +1,22 @@
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
+
+//列表
+if (obj.data?.homeV3?.elements?.edges?.length > 0) {
+    obj.data.homeV3.elements.edges = obj.data.homeV3.elements.edges.filter((i) => {
+        return !(i?.node?.hasOwnProperty('adPayload') && i.node.adPayload !== null);
+    });
+}
+
+//帖子详情
+if (obj.data?.postsInfoByIds?.length > 0) {
+    obj.data.postsInfoByIds = obj.data.postsInfoByIds.filter(
+        (i) =>
+            !(i?.hasOwnProperty("adSupplementaryTextRichtext"))
+    );
+}
+if (obj.data?.postInfoById?.pdpCommentsAds?.adPosts?.length > 0) {
+    obj.data.postInfoById.pdpCommentsAds.adPosts = [];
+}
+
+$done({ body: JSON.stringify(obj) });
