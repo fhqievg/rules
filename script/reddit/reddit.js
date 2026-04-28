5d632e47baaeb4aad1d21f3be77ca659acc1adbc
fhqievg <30724814+fhqievg@users.noreply.github.com>
2026-04-28 14:40:30 +0800

    Update reddit.js

--- script/reddit/reddit.js
+++ script/reddit/reddit.js
@@ -1,11 +1,42 @@
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-//首页-最新-列表
+const isEnableRelatedCommunities = false; //是否启用相关社区
+const isEnableTopPost = false; //是否显示置顶贴
+
+//首页相关
+//最新列表
 if (obj.data?.homeV3?.elements?.edges?.length > 0) {
-    obj.data.homeV3.elements.edges = obj.data.homeV3.elements.edges.filter((i) => {
-        return !(i?.node?.hasOwnProperty('adPayload') && i.node.adPayload !== null);
+    obj.data.homeV3.elements.edges = adDataFilter(obj.data.homeV3.elements.edges);
+}
+//新闻列表
+if (obj.data?.newsV3?.elements?.edges?.length > 0) {
+    obj.data.newsV3.elements.edges = adDataFilter(obj.data.newsV3.elements.edges);
+}
+//热门列表
+if (obj.data?.popularV3?.elements?.edges?.length > 0) {
+    obj.data.popularV3.elements.edges = adDataFilter(obj.data.popularV3.elements.edges);
+}
+
+//社区相关
+if (obj.data?.subredditV3?.elements?.edges?.length > 0) {
+    obj.data.subredditV3.elements.edges = obj.data.subredditV3.elements.edges.filter((i) => {
+        if (checkIsAd(i)) {
+            return false;
+        }
+
+        //相关社区模块处理
+        if (i?.node?.hasOwnProperty('model') && i.node.model === 'related_community_recommendations') {
+            return isEnableRelatedCommunities;
+        }
+        return true;
     });
+}
+//置顶贴处理
+if (obj.data?.subredditInfoByName?.highlightedPosts?.length > 0) {
+    if (!isEnableTopPost) {
+        obj.data.subredditInfoByName.highlightedPosts = [];
+    }
 }
 
 //帖子详情
@@ -17,6 +48,18 @@ if (obj.data?.postsInfoByIds?.length > 0) {
 }
 if (obj.data?.postInfoById?.pdpCommentsAds?.adPosts?.length > 0) {
     obj.data.postInfoById.pdpCommentsAds.adPosts = [];
+}
+
+
+function adDataFilter(list) {
+    return list.filter((i) => {
+        return !checkIsAd(i);
+    });
+}
+
+//校验是否是推广
+function checkIsAd(item, key = 'node') {
+    return (item.hasOwnProperty(key) && item[key].hasOwnProperty('adPayload') && item[key].adPayload !== null);
 }
 
 $done({ body: JSON.stringify(obj) });
