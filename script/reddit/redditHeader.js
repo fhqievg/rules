0ecb17fe01f3b5bc6821e1e5c82d85c549ddaf44
fhqievg <30724814+fhqievg@users.noreply.github.com>
2026-07-23 22:57:23 +0800

    更新 redditHeader.js

--- script/reddit/redditHeader.js
+++ script/reddit/redditHeader.js
@@ -1,6 +1,7 @@
 let headers = $request.headers;
 
-let language = 'enabled, seo, zh-hans';
+#let language = 'enabled, seo, zh-hans';
+let language = 'enabled';
 if (headers.hasOwnProperty("X-Reddit-Translations")) {
     headers['X-Reddit-Translations'] = language;
 } else {
