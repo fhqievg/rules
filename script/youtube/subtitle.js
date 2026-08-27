1fff0694fd683d9a11c1290eae3c4d282b825494
fhqievg <30724814+fhqievg@users.noreply.github.com>
2026-08-28 07:45:11 +0800

    更新 subtitle.js

--- script/youtube/subtitle.js
+++ script/youtube/subtitle.js
@@ -49,9 +49,6 @@ let options = {
 if (isQuanX) {
     options.method = 'GET';
     $task.fetch(options).then(response => {
-        console.log(response.body);
-        console.log("====================");
-        console.log(body);
         body = getResponseResult(response.body, body, setting);
         $done({ body });
     }, reason => {
