25440b6e9267ee50f9e5a073bc70fe15a19ef9d5
fhqievg <30724814+fhqievg@users.noreply.github.com>
2026-09-21 18:32:54 +0800

    更新 amap.js

--- script/amap.js
+++ script/amap.js
@@ -321,6 +321,7 @@ if (url.includes("/shield/scene/recommend")) {
             "MineUserEmblemCard", //成就勋章
             "PopularActivitiesCard", //互动专区
             "UserCircleCard", //好友动态
+            "UserCircleCardV3", //好友动态
             "UserCircleCardLite", //好友动态
             "NewUserCircleCardFeed", //好友动态
             "NewUserCircleCardFeedV2" //好友动态
